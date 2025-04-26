0f2cf200f85a6ebbc2c687b13f9f0e057bea078c
Shashwat Baheti <shashwat24baheti@gmail.com>
2025-04-27 02:13:58 +0530

    Added Google Login Functionality

--- ReciepesApp/urls.py
+++ ReciepesApp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -5,8 +6,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('home/', homepage)
-
+    path('home/', homepage, name='dashboard'),
+    path('login/', loginpage, name='login'),
+    path('user_login/', user_login, name='user_login'),
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
