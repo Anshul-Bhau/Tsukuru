6de41493d381da37a5483d1ede75d5a3d7b89752
Anshul Bhau <anshulbhau1@gmail.com>
2025-04-28 13:09:43 +0530

    minor bug fixes

--- ReciepesApp/management/commands/update_recipes_image.py
+++ ReciepesApp/management/commands/update_recipes_image.py
@@ -10,11 +10,11 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # Path to the directory where the images are located
         media_dir = os.path.join(settings.BASE_DIR, 'media', 'recipes')
-        dataset_file = 'images^Mrecipes.csv'  # Path to your dataset file
+        dataset_file = 'Food Ingredients and Recipe Dataset with Image Name Mapping.csv'  # Path to your dataset file
 
         # Open and read the dataset (assuming CSV format)
         import csv
-        with open(dataset_file, newline='') as csvfile:
+        with open(dataset_file, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 title = row['title']
@@ -24,7 +24,7 @@ class Command(BaseCommand):
                 image_path = os.path.join(media_dir, image_name)
                 if os.path.exists(image_path):
                     # Open the image file and assign it to the Recipe model
-                    with open(image_path, 'rb') as img_file:
+                    with open(image_path, 'rb', encoding="utf-8", errors='replace') as img_file:
                         recipe = Recipes(
                             title=title,
                             image=File(img_file, name=image_name)
@@ -33,3 +33,8 @@ class Command(BaseCommand):
                         self.stdout.write(self.style.SUCCESS(f"Recipe '{title}' imported successfully"))
                 else:
                     self.stdout.write(self.style.WARNING(f"Image for '{title}' not found at {image_path}"))
+# import chardet
+
+# with open("Food Ingredients and Recipe Dataset with Image Name Mapping.csv", "rb") as f:
+#     result = chardet.detect(f.read(10000))
+#     print(result)
