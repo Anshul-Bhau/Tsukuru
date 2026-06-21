de1c118d6a415510eb458c946efe69eaaa35da25
Anshul-Bhau <anshulbhau1@gmail.com>
2026-06-21 19:33:56 +0530

    Added security settings

--- ReciepesProj/settings.py
+++ ReciepesProj/settings.py
@@ -34,6 +34,16 @@ ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['127.0.0.1'])
 
 # python manage.py runserver 0.0.0.0:8000
 
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = 'DENY'
+
 # Application definition
 
 INSTALLED_APPS = [
